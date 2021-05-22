a677df1d3386a4bd44d3843aff207b48216fc24e
shahidulJahid <shahiduljahid3@gmail.com>
2021-05-22 19:45:03 +0800

    meta updated

--- src/Component/Login/firebaseConfig.js
+++ src/Component/Login/firebaseConfig.js
@@ -1,8 +1,8 @@
-export const  firebaseConfig = {
-  apiKey: "AIzaSyBDyuTEynghspCodFFidLuDL2_xcELEnho",
-  authDomain: "wedding-photographer-f7477.firebaseapp.com",
-  projectId: "wedding-photographer-f7477",
-  storageBucket: "wedding-photographer-f7477.appspot.com",
-  messagingSenderId: "97374979899",
-  appId: "1:97374979899:web:b2da208d45c86ae556591e"
+export const   firebaseConfig = {
+  apiKey: "AIzaSyBPhCa5x7Xok4BECDdB-Ldrbkcf3eU-dcU",
+  authDomain: "your--moments.firebaseapp.com",
+  projectId: "your--moments",
+  storageBucket: "your--moments.appspot.com",
+  messagingSenderId: "436315347442",
+  appId: "1:436315347442:web:7248e988955133eb28336b"
 };
